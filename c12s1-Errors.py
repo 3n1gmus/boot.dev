7a9136b99923849181ed8fe8699710b2f4fec2d1
3nigmus <wschoenly@tutanota.com>
2024-08-30 15:13:24 -0600

    init

---
+++ c12s1-Errors.py
@@ -0,0 +1,22 @@
+def main():
+    for i in range(1, 5):
+        try:
+           print(get_player_record(i))
+        except Exception as e:
+            print(e)
+
+
+# Don't edit below this line
+
+
+def get_player_record(player_id):
+    if player_id == 1:
+        return {"name": "Slayer", "level": 128}
+    if player_id == 2:
+        return {"name": "Dorgoth", "level": 300}
+    if player_id == 3:
+        return {"name": "Saruman", "level": 4000}
+    raise Exception("player id not found")
+
+
+main()
